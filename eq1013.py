4e055962b8f2fbf175bd81dac1246e7a1c583c63
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:01:19 +0100

    Update eq1013.py

--- eq1013.py
+++ eq1013.py
@@ -10,6 +10,23 @@ def eq10_13(U_A, B, M, E_MT, N):
     :param E_MT: Expression representing the function E_MT(U_A, B)
     :param N: The constant N in the equation
     :return: A tuple containing the calculated values of phi_B from M and E_MT
+
+    # Define the symbols
+    U_A = sp.symbols('U_A')
+    B = sp.symbols('B')  # Other variables that M and E_MT might depend on
+
+    # Define the constant N
+    N = sp.symbols('N')
+
+    # Define the functions M and E_MT as examples (replace with actual functions)
+    M = U_A**2 + B*U_A + 1
+    E_MT = U_A**3 - B*U_A**2 + 2
+
+    # Example usage
+    N_value = 4  # Replace with the actual value
+    phi_B_M, phi_B_E_MT = eq10_13(U_A, B, M, E_MT, N_value)
+    print("phi_B from M:", phi_B_M)
+    print("phi_B from E_MT:", phi_B_E_MT)
     """
     # Calculate the partial derivatives
     partial_M_U_A = sp.diff(M, U_A)
@@ -21,19 +38,4 @@ def eq10_13(U_A, B, M, E_MT, N):
     
     return phi_B_M, phi_B_E_MT
 
-# Define the symbols
-U_A = sp.symbols('U_A')
-B = sp.symbols('B')  # Other variables that M and E_MT might depend on
 
-# Define the constant N
-N = sp.symbols('N')
-
-# Define the functions M and E_MT as examples (replace with actual functions)
-M = U_A**2 + B*U_A + 1
-E_MT = U_A**3 - B*U_A**2 + 2
-
-# Example usage
-N_value = 4  # Replace with the actual value
-phi_B_M, phi_B_E_MT = eq10_13(U_A, B, M, E_MT, N_value)
-print("phi_B from M:", phi_B_M)
-print("phi_B from E_MT:", phi_B_E_MT)
