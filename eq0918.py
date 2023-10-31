37131650cd111fba8c2e2a032fc4ffbdf393ba1a
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:29:38 +0100

    Update eq0918.py

--- eq0918.py
+++ eq0918.py
@@ -1,15 +1,6 @@
 # Importing SymPy for symbolic mathematics
 import sympy as sp
 
-# Defining the symbol l and the functions M(l) and E_MT(l)
-l = sp.symbols('l')
-M = sp.Function('M')(l)
-E_MT = sp.Function('E_MT')(l)
-
-# Calculating the partial derivatives of M and E_MT with respect to l
-dM_dl = sp.diff(M, l)
-dE_MT_dl = sp.diff(E_MT, l)
-
 # Defining a function to evaluate the derivatives for a specific value of l
 def eq09_18(l_value):
     """
@@ -19,6 +10,15 @@ def eq09_18(l_value):
     
     :param l_value: The value of l for which the derivatives will be evaluated.
     :return: A tuple containing the evaluated derivatives of M and E_MT with respect to l.
+
+    # Defining the symbol l and the functions M(l) and E_MT(l)
+    l = sp.symbols('l')
+    M = sp.Function('M')(l)
+    E_MT = sp.Function('E_MT')(l)
+
+    # Calculating the partial derivatives of M and E_MT with respect to l
+    dM_dl = sp.diff(M, l)
+    dE_MT_dl = sp.diff(E_MT, l)
     """
     # Substituting the value of l and evaluating the derivatives
     return dM_dl.subs(l, l_value), dE_MT_dl.subs(l, l_value)
