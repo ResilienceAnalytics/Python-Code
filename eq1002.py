320a627ac3b41cec4182f90f0e51924f6a241720
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:28:44 +0100

    Update eq1002.py

--- eq1002.py
+++ eq1002.py
@@ -1,16 +1,6 @@
 # Importing SymPy for symbolic mathematics
 import sympy as sp
 
-# Defining the symbols U_P, V, N and the functions T(U_P, V), E_TM(U_P, V), and phi(V)
-U_P, V, N = sp.symbols('U_P V N')
-T = sp.Function('T')(U_P, V)
-E_TM = sp.Function('E_TM')(U_P, V)
-phi_V = sp.Function('phi')(V)
-
-# Calculating the partial derivatives of T and E_TM with respect to U_P
-dT_dUP = sp.diff(T, U_P)
-dE_TM_dUP = sp.diff(E_TM, U_P)
-
 # Defining a function to evaluate the derivatives for specific values of U_P, V, and N
 def eq10_02(U_P_value, V_value, N_value):
     """
@@ -22,6 +12,16 @@ def eq10_02(U_P_value, V_value, N_value):
     :param V_value: The value of V for which the derivatives will be evaluated.
     :param N_value: The value of N.
     :return: A tuple containing the evaluated derivatives of T and E_TM with respect to U_P, and the value of ϕ_V.
+
+    # Defining the symbols U_P, V, N and the functions T(U_P, V), E_TM(U_P, V), and phi(V)
+    U_P, V, N = sp.symbols('U_P V N')
+    T = sp.Function('T')(U_P, V)
+    E_TM = sp.Function('E_TM')(U_P, V)
+    phi_V = sp.Function('phi')(V)
+
+    # Calculating the partial derivatives of T and E_TM with respect to U_P
+    dT_dUP = sp.diff(T, U_P)
+    dE_TM_dUP = sp.diff(E_TM, U_P)
     """
     # Substituting the values of U_P, V, and N, and evaluating the derivatives
     dT_dUP_eval = dT_dUP.subs({U_P: U_P_value, V: V_value})
