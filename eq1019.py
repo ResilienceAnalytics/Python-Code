511ebab3276469e54b3fd9a5a19a4dbc3444fec3
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:55:20 +0100

    Update eq1019.py

--- eq1019.py
+++ eq1019.py
@@ -4,10 +4,12 @@ def eq10_19(gamma_MT):
 
     :param gamma_MT: Value of gamma_MT
     :return: Boolean value indicating if gamma_MT is greater than 1
+
+    # Example usage
+    gamma_MT_value = 1.5  # Replace with the actual value
+    result = eq10_19(gamma_MT_value)
+    print("Is gamma_MT greater than 1?", result)
     """
     return gamma_MT > 1
 
-# Example usage
-gamma_MT_value = 1.5  # Replace with the actual value
-result = eq10_19(gamma_MT_value)
-print("Is gamma_MT greater than 1?", result)
+
