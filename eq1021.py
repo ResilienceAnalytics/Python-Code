6e0132c4173f7eddfc0ff0bd8c42b5a0b12b2d0f
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:54:37 +0100

    Update eq1021.py

--- eq1021.py
+++ eq1021.py
@@ -8,13 +8,15 @@ def eq10_21(U_A, B, gamma_MT):
     :param B: Value of B
     :param gamma_MT: Value of gamma_MT
     :return: Calculated value of k_UB
+
+    # Example usage
+    U_A_value = 4       # Replace with the actual value
+    B_value = 2         # Replace with the actual value
+    gamma_MT_value = 3  # Replace with the actual value
+    k_UB = eq10_21(U_A_value, B_value, gamma_MT_value)
+    print("k_UB:", k_UB)
     """
     k_UB = U_A * np.power(B, gamma_MT - 1)
     return k_UB
 
-# Example usage
-U_A_value = 4       # Replace with the actual value
-B_value = 2         # Replace with the actual value
-gamma_MT_value = 3  # Replace with the actual value
-k_UB = eq10_21(U_A_value, B_value, gamma_MT_value)
-print("k_UB:", k_UB)
+
