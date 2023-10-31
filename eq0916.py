464ced0708d2b677b2ad8e435b8bfe021ab39849
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:30:22 +0100

    Update eq0916.py

--- eq0916.py
+++ eq0916.py
@@ -1,15 +1,6 @@
 # Importing SymPy for symbolic mathematics
 import sympy as sp
 
-# Defining the symbol p and the functions T(p) and E_TM(p)
-p = sp.symbols('p')
-T = sp.Function('T')(p)
-E_TM = sp.Function('E_TM')(p)
-
-# Calculating the partial derivatives of T and E_TM with respect to p
-dT_dp = sp.diff(T, p)
-dE_TM_dp = sp.diff(E_TM, p)
-
 # Defining a function to evaluate the derivatives for a specific value of p
 def eq09_16(p_value):
     """
@@ -19,6 +10,15 @@ def eq09_16(p_value):
     
     :param p_value: The value of p for which the derivatives will be evaluated.
     :return: A tuple containing the evaluated derivatives of T and E_TM with respect to p.
+
+    # Defining the symbol p and the functions T(p) and E_TM(p)
+    p = sp.symbols('p')
+    T = sp.Function('T')(p)
+    E_TM = sp.Function('E_TM')(p)
+
+    # Calculating the partial derivatives of T and E_TM with respect to p
+    dT_dp = sp.diff(T, p)
+    dE_TM_dp = sp.diff(E_TM, p)
     """
     # Substituting the value of p and evaluating the derivatives
     return dT_dp.subs(p, p_value), dE_TM_dp.subs(p, p_value)
