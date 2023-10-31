7fa48e4d47a7476c72b5df88ff631cba08f59420
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:03:52 +0100

    Update eq1007.py

--- eq1007.py
+++ eq1007.py
@@ -8,6 +8,14 @@ def eq10_07(phi_F_A, phi_V):
     :param phi_F_A: The value for Phi_F_A, a parameter in the equation
     :param phi_V: The value for Phi_V, another parameter in the equation
     :return: The calculated value of gamma_TM
+
+    # Example usage:
+
+    phi_F_A_value = 10
+    phi_V_value = 5
+    result = eq10_07(phi_F_A_value, phi_V_value)
+    print("Result of eq10-07:", result)  
+    # Expected output: 2.0
     """
     
     # Ensure that the input values are either numbers or SymPy expressions
@@ -23,10 +31,4 @@ def eq10_07(phi_F_A, phi_V):
     
     return gamma_TM
 
-# Example usage:
 
-#phi_F_A_value = 10
-#phi_V_value = 5
-#result = eq10_07(phi_F_A_value, phi_V_value)
-#print("Result of eq10-07:", result)  
-# Expected output: 2.0
