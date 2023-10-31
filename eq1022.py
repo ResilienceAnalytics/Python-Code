abbe3383604c65edd26bd2e8a3fea933b32f6f91
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:54:14 +0100

    Update eq1022.py

--- eq1022.py
+++ eq1022.py
@@ -8,13 +8,15 @@ def eq10_22(U_A, F_P, gamma_MT):
     :param F_P: Value of F_P
     :param gamma_MT: Value of gamma_MT
     :return: Calculated value of k_UF
-    """
+
+    # Example usage
+    U_A_value = 4       # Replace with the actual value
+    F_P_value = 2       # Replace with the actual value
+    gamma_MT_value = 3  # Replace with the actual value
+    k_UF = eq10_22(U_A_value, F_P_value, gamma_MT_value)
+    print("k_UF:", k_UF)
+        """
     k_UF = np.power(U_A, gamma_MT) * np.power(F_P, 1 - gamma_MT)
     return k_UF
 
-# Example usage
-#U_A_value = 4       # Replace with the actual value
-#F_P_value = 2       # Replace with the actual value
-#gamma_MT_value = 3  # Replace with the actual value
-#k_UF = eq10_22(U_A_value, F_P_value, gamma_MT_value)
-#print("k_UF:", k_UF)
+
