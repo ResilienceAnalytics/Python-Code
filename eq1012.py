a192d68fe539cac23127975345048cd9b1518d44
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:01:57 +0100

    Update eq1012.py

--- eq1012.py
+++ eq1012.py
@@ -9,6 +9,19 @@ def eq10_12(U_A, B, M, E_MT):
     :param M: Expression representing the function M(U_A, B)
     :param E_MT: Expression representing the function E_MT(U_A, B)
     :return: A tuple containing the calculated values of Phi_B from M and E_MT
+
+    # Define the symbols
+    U_A = sp.symbols('U_A')
+    B = sp.symbols('B')  # Other variables that M and E_MT might depend on
+
+    # Define the functions M and E_MT as examples (replace with actual functions)
+    M = U_A**2 + B*U_A + 1
+    E_MT = U_A**3 - B*U_A**2 + 2
+
+    # Example usage
+    Phi_B_M, Phi_B_E_MT = eq10_12(U_A, B, M, E_MT)
+    print("Phi_B from M:", Phi_B_M)
+    print("Phi_B from E_MT:", Phi_B_E_MT)
     """
     # Calculate the partial derivatives
     partial_M_U_A = sp.diff(M, U_A)
@@ -20,15 +33,4 @@ def eq10_12(U_A, B, M, E_MT):
     
     return Phi_B_M, Phi_B_E_MT
 
-# Define the symbols
-U_A = sp.symbols('U_A')
-B = sp.symbols('B')  # Other variables that M and E_MT might depend on
 
-# Define the functions M and E_MT as examples (replace with actual functions)
-M = U_A**2 + B*U_A + 1
-E_MT = U_A**3 - B*U_A**2 + 2
-
-# Example usage
-Phi_B_M, Phi_B_E_MT = eq10_12(U_A, B, M, E_MT)
-print("Phi_B from M:", Phi_B_M)
-print("Phi_B from E_MT:", Phi_B_E_MT)
