bc4ae4482bebfbd1bfea0fcbcf72362987261031
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:23:40 +0100

    Update eq1006.py

--- eq1006.py
+++ eq1006.py
@@ -1,16 +1,28 @@
 # Importing SymPy for symbolic mathematics
 import sympy as sp
 
-# Defining the symbols F_A, V, and U_P
-F_A, V, U_P = sp.symbols('F_A V U_P', positive=True, real=True)
-
-# Defining the equation (partial F_A / F_A) + (partial V / V) = (partial U_P / U_P)
-eq = sp.Eq(sp.diff(F_A)/F_A + sp.diff(V)/V, sp.diff(U_P)/U_P)
-
-# Solving the equation for U_P (optional)
-solution = sp.solve(eq, U_P)
-
-# Adding comments to explain each step
-# Here, we have defined a symbolic equation based on the provided expression.
-# The symbols F_A, V, and U_P are assumed to be positive real numbers.
-# The equation is then solved for U_P, but you can also solve it for other variables if needed.
+def eq1006(F_A_value, V_value):
+    """
+    Solve the equation (partial F_A / F_A) + (partial V / V) = (partial U_P / U_P)
+    with respect to U_P, given specific values for F_A and V.
+    
+    Parameters:
+    F_A_value (float or int): The value to substitute for F_A in the equation.
+    V_value (float or int): The value to substitute for V in the equation.
+    
+    Returns:
+    list: A list of solutions for U_P.
+    """
+    # Defining the symbols F_A, V, and U_P
+    F_A, V, U_P = sp.symbols('F_A V U_P', positive=True, real=True)
+    
+    # Defining the equation (partial F_A / F_A) + (partial V / V) = (partial U_P / U_P)
+    eq = sp.Eq(sp.diff(F_A)/F_A + sp.diff(V)/V, sp.diff(U_P)/U_P)
+    
+    # Substituting the given values for F_A and V
+    eq_substituted = eq.subs({F_A: F_A_value, V: V_value})
+    
+    # Solving the substituted equation for U_P
+    solution = sp.solve(eq_substituted, U_P)
+    
+    return solution
