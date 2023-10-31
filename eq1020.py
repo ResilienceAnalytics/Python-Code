2de3a11c4d118992ec772ae6a780d16f824de023
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:55:00 +0100

    Update eq1020.py

--- eq1020.py
+++ eq1020.py
@@ -8,13 +8,14 @@ def eq10_20(F_P, B, gamma_MT):
     :param B: Value of B
     :param gamma_MT: Value of gamma_MT
     :return: Calculated value of k_FB
+    
+    # Example usage
+    F_P_value = 4       # Replace with the actual value
+    B_value = 2         # Replace with the actual value
+    gamma_MT_value = 3  # Replace with the actual value
+    k_FB = eq10_20(F_P_value, B_value, gamma_MT_value)
+    print("k_FB:", k_FB)
     """
     k_FB = F_P * np.power(B, gamma_MT)
     return k_FB
 
-# Example usage
-F_P_value = 4       # Replace with the actual value
-B_value = 2         # Replace with the actual value
-gamma_MT_value = 3  # Replace with the actual value
-k_FB = eq10_20(F_P_value, B_value, gamma_MT_value)
-print("k_FB:", k_FB)
