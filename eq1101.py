e75d3ab33fb7737df7e7d070b2e367af33691b38
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:53:41 +0100

    Update eq1101.py

--- eq1101.py
+++ eq1101.py
@@ -5,12 +5,14 @@ def eq11_01(delta_T_lost, delta_T_useful):
     :param delta_T_lost: Change in temperature due to losses
     :param delta_T_useful: Useful change in temperature
     :return: Total change in temperature
+    Example usage
+    delta_T_lost_value = 4       # Replace with the actual value
+    delta_T_useful_value = 2     # Replace with the actual value
+    delta_T_total = eq11_01(delta_T_lost_value, delta_T_useful_value)
+    print("Delta T Total:", delta_T_total)
+    
     """
     delta_T_total = delta_T_lost + delta_T_useful
     return delta_T_total
 
-#Example usage
-#delta_T_lost_value = 4       # Replace with the actual value
-#delta_T_useful_value = 2     # Replace with the actual value
-#delta_T_total = eq11_01(delta_T_lost_value, delta_T_useful_value)
-#print("Delta T Total:", delta_T_total)
+
