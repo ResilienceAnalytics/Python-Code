1a33476fab08235c4a82a552484337b6017e32c0
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 13:03:21 +0100

    Update eq1008.py

--- eq1008.py
+++ eq1008.py
@@ -4,6 +4,13 @@ def eq10_08(gamma_TM):
 
     :param gamma_TM: The value of gamma_TM to be checked
     :return: True if gamma_TM is less than 1, False otherwise
+
+    
+    # Example usage:
+    gamma_TM_value = 0.5
+    result = eq10_08(gamma_TM_value)
+    print("Is gamma_TM < 1?", result)  
+    # Expected output: True
     """
     # Ensure that the input value is a number or a SymPy expression
     gamma_TM = sp.sympify(gamma_TM)
@@ -11,8 +18,3 @@ def eq10_08(gamma_TM):
     # Check if gamma_TM is less than 1
     return gamma_TM < 1
 
-# Example usage:
-#gamma_TM_value = 0.5
-#result = eq10_08(gamma_TM_value)
-#print("Is gamma_TM < 1?", result)  
-# Expected output: True
