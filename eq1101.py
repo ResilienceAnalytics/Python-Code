0f00c96ef62711330188bff2c2a0f4dfde137b69
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-10-31 12:12:11 +0100

    Update eq1101.py

--- eq1101.py
+++ eq1101.py
@@ -9,8 +9,8 @@ def eq11_01(delta_T_lost, delta_T_useful):
     delta_T_total = delta_T_lost + delta_T_useful
     return delta_T_total
 
-# Example usage
-delta_T_lost_value = 4       # Replace with the actual value
-delta_T_useful_value = 2     # Replace with the actual value
-delta_T_total = eq11_01(delta_T_lost_value, delta_T_useful_value)
-print("Delta T Total:", delta_T_total)
+#Example usage
+#delta_T_lost_value = 4       # Replace with the actual value
+#delta_T_useful_value = 2     # Replace with the actual value
+#delta_T_total = eq11_01(delta_T_lost_value, delta_T_useful_value)
+#print("Delta T Total:", delta_T_total)
