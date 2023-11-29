c4cce92b3bbec5e2fcd6d17bcb07967c45e54229
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-11-29 21:08:39 +0100

    Update TokenizeNClassify.py

--- TokenizeNClassify.py
+++ TokenizeNClassify.py
@@ -1,17 +1,14 @@
-# Importing necessary libraries
-import pdfplumber  # Used for extracting text from PDF files
-import nltk  # Natural Language Toolkit for text processing
-from nltk.tokenize import word_tokenize  # For tokenizing text into words
-from transformers import BertTokenizer  # Advanced tokenizer from Hugging Face
-from PIL import Image  # Python Imaging Library for image processing
-import io  # Input/output module for handling byte streams
-import re  # Regular expressions library
-import sys  # System-specific parameters and functions
+import pdfplumber
+import nltk
+from nltk.tokenize import word_tokenize
+from transformers import BertTokenizer
+from PIL import Image
+import io
+import re
+import sys
 
-# Download and initialize the NLTK tokenizer
+# Initialisation des tokenizers
 nltk.download('punkt')
-
-# Initialize the BERT tokenizer
 bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 # Define a function to normalize text
@@ -29,51 +26,99 @@ def normalize_text(text):
     text = re.sub(r'[^a-z0-9\s]', '', text)  # Remove non-alphanumeric characters
     return text
 
-# Define a function to extract text and images from a PDF
+
+class DomainClassifier:
+    """
+    A classifier that categorizes terms into 'Natural Sciences' or 'Human Sciences'.
+
+    Attributes:
+        dataset (dict): A dictionary mapping terms to their corresponding domains.
+    """
+    def __init__(self):
+        self.dataset = {
+            # Terms for Creation-Natural Science sequence
+            "creation": "Natural Sciences", "manufacture": "Natural Sciences",
+            "elaboration": "Natural Sciences", "invention": "Natural Sciences",
+            "conception": "Natural Sciences", "genesis": "Natural Sciences",
+            "initiation": "Natural Sciences", "production": "Natural Sciences",
+            "fabrication": "Natural Sciences", "assemblage": "Natural Sciences",
+            "construction": "Natural Sciences", "development": "Natural Sciences",
+            "refinement": "Natural Sciences", "expansion": "Natural Sciences",
+            "amplification": "Natural Sciences",
+
+            # Terms for Distribution-Human Science sequence
+            "distribution": "Human Sciences", "allocation": "Human Sciences",
+            "ventilation": "Human Sciences", "dissemination": "Human Sciences",
+            "dispersion": "Human Sciences", "apportionment": "Human Sciences",
+            "diffusion": "Human Sciences", "assignation": "Human Sciences",
+            "allotment": "Human Sciences", "rationing": "Human Sciences",
+            "designation": "Human Sciences", "circulation": "Human Sciences",
+            "broadcasting": "Human Sciences", "dispersal": "Human Sciences",
+            "propagation": "Human Sciences",
+
+            # Terms for Use-Natural Science sequence
+            "use": "Natural Sciences", "functioning": "Natural Sciences",
+            "usage": "Natural Sciences"
+        }
+
+    def classify_term(self, term):
+        """
+        Classify a given term according to its domain.
+
+        Args:
+            term (str): The term to classify.
+
+        Returns:
+            str: The domain of the term.
+        """
+        return self.dataset.get(term, "Unknown")
+
+def tokenize_text(text, method):
+    """
+    Tokenize the text using tokenizer BERT.
+    """
+    text = normalize_text(text)
+    lines = text.split('\n')
+    tokenized_content = [tokenizer.tokenize(line) for line in lines]
+    return tokenized_content
+
 def extract_text_and_images_from_pdf(pdf_path):
     """
-    Extract text and images from the given PDF file.
-
+    Extracts text and images from a PDF file.
+    
     Args:
-        pdf_path (str): File path of the PDF.
+    pdf_path (str): The file path of the PDF to be processed.
 
     Returns:
-        tuple: A tuple containing the extracted text and a list of images.
+    tuple: Returns two items; a string containing all the text extracted from the PDF, 
+           and a list of images found in the PDF.
     """
-    try:
-        text = ""  # Initialize an empty string for text
-        images = []  # Initialize an empty list for images
-        with pdfplumber.open(pdf_path) as pdf:  # Open the PDF
-            for page in pdf.pages:  # Iterate through each page
-                page_text = page.extract_text()  # Extract text from the current page
-                if page_text:  # If text is found
-                    text += page_text + "\n"  # Append it to the text string
-                images.extend(page.images)  # Append images from the current page
-        return text, images  # Return the extracted text and images
-    except Exception as e:
-        raise RuntimeError(f"Error while extracting PDF: {e}")
+    text = ""
+    images = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+            images.extend(page.images)
+    return text, images
 
-# Define a function to apply OCR to images
 def apply_ocr_to_images(images):
     """
-    Apply Optical Character Recognition (OCR) on the list of images.
+    Applies Optical Character Recognition (OCR) to a list of images.
 
     Args:
-        images (list): List of images to process.
+    images (list): A list of images to apply OCR on.
 
     Returns:
-        list: A list of text strings extracted from the images.
+    list: A list of strings where each string is the OCR result of an image.
     """
-    try:
-        ocr_results = []  # Initialize an empty list for OCR results
-        for img in images:  # Iterate through each image
-            im = Image.open(io.BytesIO(img['stream']))  # Open the image
-            ocr_results.append(pytesseract.image_to_string(im))  # Apply OCR and append the result
-        return ocr_results  # Return the OCR results
-    except Exception as e:
-        raise RuntimeError(f"Error while applying OCR: {e}")
+    ocr_results = []
+    for img in images:
+        im = Image.open(io.BytesIO(img['stream']))
+        ocr_results.append(pytesseract.image_to_string(im))
+    return ocr_results
 
-# Define a function to tokenize text based on the chosen method
 def tokenize_text(text, method):
     """
     Tokenize the given text based on the specified method.
@@ -104,7 +149,21 @@ def tokenize_text(text, method):
     except Exception as e:
         raise RuntimeError(f"Error while tokenizing: {e}")
 
-# Define the main function for script execution
+def tokenize_and_classify(text, method, classifier):
+    """
+    Tokenize and classify terms in the given text.
+
+    Args:
+        text (str): The text to process.
+        classifier (DomainClassifier): The classifier to use for categorizing terms.
+
+    Returns:
+        list: A list of tuples where each tuple contains a token and its domain.
+    """
+    tokenized_content = tokenize_text(text, method)
+    classified_tokens = [(token, classifier.classify_term(token)) for line in tokenized_content for token in line]
+    return classified_tokens
+
 def main():
     """
     Main function to extract text from a PDF, apply OCR, tokenize the text,
@@ -114,28 +173,20 @@ def main():
         print("Usage: python script.py <input_pdf_file> <tokenization_method>")
         sys.exit(1)
 
-    pdf_path = sys.argv[1]  # Get the PDF file path from command line arguments
-    method = sys.argv[2]  # Get the tokenization method
-    output_path = pdf_path.rsplit('.', 1)[0] + "_tokenizer.txt"  # Output file path
+    pdf_path = sys.argv[1]
+    method = sys.argv[2]
+    output_path = pdf_path.rsplit('.', 1)[0] + "_tokenized_classified.txt"
 
-    # Extract text and images from the PDF
     extracted_text, extracted_images = extract_text_and_images_from_pdf(pdf_path)
-
-    # Apply OCR to the images
     ocr_results = apply_ocr_to_images(extracted_images)
+    classifier = DomainClassifier()
+    tokenized_classified_text = tokenize_and_classify(extracted_text + ' '.join(ocr_results), method, classifier)
 
-    # Tokenize the extracted text
-    tokenized_text = tokenize_text(extracted_text, method)
-
-    # Save the tokenized content to the output file
     with open(output_path, 'w') as f:
-        for line in tokenized_text:
-            f.write(' '.join(line) + '\n')
-        for line in ocr_results:
-            f.write(normalize_text(line) + '\n')
+        for token, domain in tokenized_classified_text:
+            f.write(f"{token} - {domain}\n")
 
-    print(f"Tokenized content written to {output_path}")
+    print(f"Tokenized and classified content written to {output_path}")
 
-# Execute the main function when the script is run
 if __name__ == "__main__":
     main()
