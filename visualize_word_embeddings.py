74573343fe0cd0a0115ebc10b5cffb907f50a1f0
Hal149 <138213516+ResilienceAnalytics@users.noreply.github.com>
2023-12-24 18:25:43 +0100

    Update visualize_word_embeddings.py

--- visualize_word_embeddings.py
+++ visualize_word_embeddings.py
@@ -1,64 +1,44 @@
-import argparse
-import json
-import matplotlib
-matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from transformers import BertTokenizer, BertModel
 import torch
+import numpy as np
 
-def visualize_word_embeddings_from_file(json_filepath, n_components, random_state):
-    """
-    Visualize the construction of sentence embeddings word by word from a JSON file.
-    
-    Args:
-        json_filepath (str): The filepath of the JSON file containing tokenized words.
-        n_components (int): The number of dimensions for t-SNE.
-        random_state (int): The random state for t-SNE.
-    """
-    
-    # Load tokenized words from JSON file
-    with open(json_filepath, 'r') as file:
-        tokenized_sentences = json.load(file)
-
-    # Initialize tokenizer and model
+def visualize_semantic_development(sentence, n_components, random_state):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained('bert-base-uncased')
 
-    # Process each list of tokens
-    for tokens in tokenized_sentences:
-        sentence = ' '.join(tokens)
-        print(f"Visualizing tokens: {tokens}")
+    tokenized_sentence = tokenizer.tokenize(sentence)
+    cumulative_embeddings = []
 
-        encoded_input = tokenizer(sentence, return_tensors='pt')
-        input_ids = encoded_input['input_ids']
+    for i in range(len(tokenized_sentence)):
+        partial_sentence = ' '.join(tokenized_sentence[:i+1])
+        encoded_input = tokenizer(partial_sentence, return_tensors='pt')
         with torch.no_grad():
             outputs = model(**encoded_input)
-        last_hidden_states = outputs.last_hidden_state.squeeze(0)
-
-        # Adjust perplexity based on the length of tokens
-        perplexity = min(30, len(tokens)-1)  # Ensure perplexity is less than the number of tokens
-
-        tsne = TSNE(n_components=n_components, random_state=random_state, perplexity=perplexity)
-        word_embeddings_2d = tsne.fit_transform(last_hidden_states.numpy())
-
-        plt.figure(figsize=(12, 8))
-        for i, token_id in enumerate(input_ids[0]):
-            token = tokenizer.decode([token_id])
-            plt.scatter(word_embeddings_2d[i, 0], word_embeddings_2d[i, 1])
-            plt.annotate(token, (word_embeddings_2d[i, 0], word_embeddings_2d[i, 1]))
+        embeddings = outputs.last_hidden_state.squeeze(0).numpy()
         
-        plt.title(f"t-SNE visualization of the tokens '{tokens}'")
-        plt.xlabel("t-SNE feature 1")
-        plt.ylabel("t-SNE feature 2")
-        plt.savefig(f"{json_filepath}_{tokens}_visualization.png")
-        plt.close()
+        cumulative_embeddings.extend(embeddings[-1:])
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("n_components", type=int, help="Number of dimensions for t-SNE")
-    parser.add_argument("random_state", type=int, help="Random state for t-SNE")
-    parser.add_argument("json_filepath", type=str, help="Path to the JSON file with tokenized words")
-    args = parser.parse_args()
+        if len(cumulative_embeddings) > 1:
+            cumulative_embeddings_np = np.array(cumulative_embeddings)
 
-    visualize_word_embeddings_from_file(args.json_filepath, args.n_components, args.random_state)
+            # Ajustez la perplexité en fonction du nombre d'embeddings
+            perplexity = min(len(cumulative_embeddings) - 1, 30)
+            
+            tsne = TSNE(n_components=n_components, random_state=random_state, perplexity=perplexity)
+            embeddings_2d = tsne.fit_transform(cumulative_embeddings_np)
+
+            plt.figure(figsize=(12, 8))
+            plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+            for j, word in enumerate(tokenized_sentence[:i+1]):
+                plt.annotate(word, (embeddings_2d[j, 0], embeddings_2d[j, 1]))
+            plt.title(f"Semantic development up to word: '{word}'")
+            plt.xlabel("t-SNE feature 1")
+            plt.ylabel("t-SNE feature 2")
+            plt.savefig(f"semantic_development_{i}.png")
+            plt.close()
+
+# Exemple d'utilisation
+sentence = "The quick brown fox jumps over the lazy dog"
+visualize_semantic_development(sentence, 2, 42)
